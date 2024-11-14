c116e5a80f80c24ebff8d146c4dc654bee42dc1b
drkaputa <dskiee@rit.edu>
2024-11-13 20:08:19 -0500

    model that fits sizing but fails timing.

---
+++ frameBased/sizing/fiSetup.m
@@ -0,0 +1,7 @@
+fimath('ProductMode','SpecifyPrecision','SumMode','SpecifyPrecision',SumFractionLength=15,ProductFractionLength=15)
+
+fimath('ProductMode','SpecifyPrecision','RoundingMethod','Zero','SumMode','SpecifyPrecision',SumFractionLength=15,ProductFractionLength=15)
+
+fimath('ProductMode','SpecifyPrecision','RoundingMethod','Zero','SumMode','SpecifyPrecision',SumFractionLength=15,ProductFractionLength=15)
+
+fimath('ProductMode','SpecifyPrecision','RoundingMethod','Zero','SumMode','SpecifyPrecision',SumFractionLength=32,ProductFractionLength=32,ProductWordLength=64,SumWordLength=64)
